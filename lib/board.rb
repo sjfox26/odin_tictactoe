8965b226c487bcf0d646d7f3c998fa5e3cb9f97c
Scott Fox <scottfox87@gmail.com>
2017-11-27 15:12:00 -0600

    Add Board#draw_board test. Make test pass

--- lib/board.rb
+++ lib/board.rb
@@ -5,4 +5,10 @@ class Board
     @cell = [ [1,2,3], [4,5,6], [7,8,9] ]
   end
 
+  def draw_board
+    puts "#{cell[0][0]} | #{cell[0][1]} | #{cell[0][2]}"
+    puts "#{cell[1][0]} | #{cell[1][1]} | #{cell[1][2]}"
+    puts "#{cell[2][0]} | #{cell[2][1]} | #{cell[2][2]}"
+  end
+
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -9,5 +9,13 @@ RSpec.describe Board do
     end
   end
 
+  context "#draw_board" do
+    it "draws the game board" do
+      board = Board.new
+      matrix_format_str = "1 | 2 | 3\n4 | 5 | 6\n7 | 8 | 9\n"
+      expect { board.draw_board }.to output(matrix_format_str).to_stdout
+    end
+  end
+
 
 end
